92e07e372e02d27cafd9ff119173eba71fa15738
alaaaldabak <alaa_aldabak@hotmail.com>
2021-08-08 20:23:00 +0300

    myfinalproject

---
+++ src/EvBilgileri.java
@@ -0,0 +1,101 @@
+import java.lang.*;
+public class EvBilgileri {
+    int homeNo;
+    String homeCity,homeDistrict,homeAdress;
+    float homeSquare,homePrice;
+    int homeRooms,homeFloor,homeOld;
+    int homeStatus;
+
+    public EvBilgileri(int homeNo, String homeCity, String homeDistrict, String homeAdress, float homeSquare, float homePrice, int homeRooms, int homeFloor, int homeOld, int homeStatus) {
+        this.homeNo = homeNo;
+        this.homeCity = homeCity;
+        this.homeDistrict = homeDistrict;
+        this.homeAdress = homeAdress;
+        this.homeSquare = homeSquare;
+        this.homePrice = homePrice;
+        this.homeRooms = homeRooms;
+        this.homeFloor = homeFloor;
+        this.homeOld = homeOld;
+        this.homeStatus = homeStatus;
+    }
+
+    public int getHomeNo() {
+        return homeNo;
+    }
+
+    public String getHomeCity() {
+        return homeCity;
+    }
+
+    public String getHomeDistrict() {
+        return homeDistrict;
+    }
+
+    public String getHomeAdress() {
+        return homeAdress;
+    }
+
+    public float getHomeSquare() {
+        return homeSquare;
+    }
+
+    public float getHomePrice() {
+        return homePrice;
+    }
+
+    public int getHomeRooms() {
+        return homeRooms;
+    }
+
+    public int getHomeFloor() {
+        return homeFloor;
+    }
+
+    public int getHomeOld() {
+        return homeOld;
+    }
+
+    public int isHomeStatus() {
+        return homeStatus;
+    }
+
+    public void setHomeNo(int homeNo) {
+        this.homeNo = homeNo;
+    }
+
+    public void setHomeCity(String homeCity) {
+        this.homeCity = homeCity;
+    }
+
+    public void setHomeDistrict(String homeDistrict) {
+        this.homeDistrict = homeDistrict;
+    }
+
+    public void setHomeAdress(String homeAdress) {
+        this.homeAdress = homeAdress;
+    }
+
+    public void setHomeSquare(float homeSquare) {
+        this.homeSquare = homeSquare;
+    }
+
+    public void setHomePrice(float homePrice) {
+        this.homePrice = homePrice;
+    }
+
+    public void setHomeRooms(int homeRooms) {
+        this.homeRooms = homeRooms;
+    }
+
+    public void setHomeFloor(int homeFloor) {
+        this.homeFloor = homeFloor;
+    }
+
+    public void setHomeOld(int homeOld) {
+        this.homeOld = homeOld;
+    }
+
+    public void setHomeStatus(int homeStatus) {
+        this.homeStatus = homeStatus;
+    }
+}
---
+++ src/FinalProjesiEmlakci.java
@@ -0,0 +1,131 @@
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class FinalProjesiEmlakci {
+     static String userName="Alaa",password="123";
+    static List<EvBilgileri> homeDetailes = new ArrayList<>() ;
+
+    public static void main(String[] args) {
+
+        Scanner input = new Scanner(System.in);
+        String userInput, passInput;
+        // test kullanıcı Adı veya Şifresi doğru mu ?;
+        do {
+            System.out.println("Lütfen Kullanıcı Adı giriniz:");
+            userInput = input.nextLine();
+            System.out.println("Lütfen  Kullanıcı Şifresi giriniz:");
+            passInput = input.nextLine();
+            if (!isUserNameAndPasswordTrue(userInput, passInput)) {
+                System.out.println("Yanlış girdiğiniz Kullanıcı Adı yada Şifresi! Lütfen tekrar giriniz!");
+            } else
+                break;
+
+        } while (true);
+        // Yeni Ev Ekleme
+        while (true){
+
+            if (selectionNo() == 1) {
+                addHome();
+            } else if (selectionNo() == 2) {
+                searchHome();
+
+
+            } else if (selectionNo() == 3) {
+
+            } else {
+                System.out.println("Lütfen doğru bir operasyon seçiniz!");
+            }
+        }
+
+    }
+    // fonksyoun  kullanıcı Adı veya Şifresi doğru mu ?;
+    public static boolean isUserNameAndPasswordTrue(String userInput, String passInput){
+        boolean isTrue = false;
+        if(userInput.equals(userName) && (passInput.equals(password)) ){
+            isTrue = true;
+        }
+        return isTrue;
+    }
+    // listedenki seçenek olacak
+    public static int selectionNo(){
+        Scanner select = new Scanner(System.in);
+        boolean isNumberWrong = true;
+        String inputCheck = "";
+        while (isNumberWrong) {
+            System.out.println("Lütfen birini seçiniz:\n 1. Ev Ekleme \n 2. Ev Sorgulama \n 3. Ev Satılan veya Kiralananlar\n ");
+            try {
+                inputCheck = select.next();
+                if (Integer.class.isInstance(Integer.parseInt(inputCheck))) {
+
+                    if (inputCheck.equals("1") || inputCheck.equals("2") || inputCheck.equals("3")) {
+                        isNumberWrong = false;
+                    } else {
+                        System.out.println("Lütfen 1 yada 2 yada 3 seçiniz!");
+                    }
+                }
+            } catch (Exception e) {
+                System.out.println("Girdiğinz sayı doğru değildir.Lütfen tekrar deneyiniz!");
+            }
+        }
+        return Integer.parseInt(inputCheck);
+
+    }
+    //fonksyoun yeni ev eklemek için
+    public static void addHome(){
+        Scanner input = new Scanner(System.in);
+        System.out.println("Lütfen yeni Ev Bilgileri giriniz:");
+        // int homeNo
+        System.out.println("İl :");
+        String homeCity = input.nextLine();
+        System.out.println("İlçe :");
+        String homeDistrict = input.nextLine();
+        System.out.println("Adres :");
+        String homeAdress = input.nextLine();
+        System.out.println("Ev Alanı :");
+        float homeSquare = input.nextFloat();
+        System.out.println("Ev Fıyatı :");
+        float  homePrice= input.nextFloat();
+        System.out.println("Ev Oda Sayısı :");
+        int homeRooms = input.nextInt();
+        System.out.println("Ev Katı :");
+        int homeFloor = input.nextInt();
+        System.out.println("Ev Yaşı :");
+        int homeOld = input.nextInt();
+        System.out.println("Ev eğer kiralamak 0'e pasınız, satılık için 1'e pasınız!");
+        int homeStatus = input.nextInt();
+        homeDetailes.add(new EvBilgileri( homeDetailes.size()+1,  homeCity,  homeDistrict,  homeAdress,  homeSquare,  homePrice,  homeRooms,  homeFloor,  homeOld,homeStatus));
+
+
+    }
+    //fonksyoun  ev sorgulama için
+    public static void searchHome(){
+        System.out.println("Ev sorgulamak için hangisine sorgulacaksin:");
+        System.out.println("İl seçiniz:");
+        List<String> city = new ArrayList<>();
+        boolean found = false;
+        for (EvBilgileri evfound : homeDetailes){
+            String cityName = evfound.homeCity;
+            if (city.size()!=0){
+             for (String cityName1 : city){
+                 if (cityName1.equals(cityName)){
+                     found = true;
+                     break;
+                 }
+             }
+
+           } else{
+                city.add(evfound.homeCity);
+            }
+          if(!found)
+                city.add(evfound.homeCity);
+
+
+//            System.out.println("Ev numarası: "+evfound.homeNo+"\n"+"Il: "+evfound.homeCity+" Ilçe: "+evfound.homeDistrict+
+//                    " Kat Numarası: "+evfound.homeFloor+" Ev Toplam Alanı:" + evfound.homeSquare+" ");
+        }for (String cityName1 : city){
+            System.out.println(cityName1+"\n");
+        }
+
+    }
+}
